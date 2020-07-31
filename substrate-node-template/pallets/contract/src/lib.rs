cf33d71551f18016833347aee7ea9b02bd100427
AmadeusGB <1069830494@qq.com>
2020-07-31 10:07:32 +0800

    version1.1.2 for update contract

--- substrate-node-template/pallets/contract/src/lib.rs
+++ substrate-node-template/pallets/contract/src/lib.rs
@@ -29,10 +29,10 @@ pub trait Trait: system::Trait {
 pub struct ContractT<T: Trait> {
 	pub ps_addr: T::AccountId,										//合同PS地址(通过地址和ID取得VPP所有信息)
 	pub vpp_number: u64,											 //该地址下虚拟电厂ID
-	pub block_number: T::BlockNumber,				   //合同成交时区块号
+	pub block_number: T::BlockNumber,				   //合同创建时区块号
 	pub contract_price: BalanceOf<T>,		  			 //合同总价
 	pub energy_amount: u64,							  			 //购买电能度数
-	pub execution_status:u8,									  //合同执行状态
+	pub execution_status:u8,									  //合同执行状态（执行中：1，已完成：2，已终止：3）
 	pub contract_type:bool,								 		   //合同分类（购买/出售）
 	pub ammeter_id: Vec<u8>,						 		   //电表编号
 }
@@ -40,17 +40,17 @@ pub struct ContractT<T: Trait> {
 // This pallet's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
-		Something get(fn something): Option<u32>;
-
-		Contracts get(fn vpps): map hasher(blake2_128_concat) (T::AccountId, u64) => Option<ContractT<T>>;
-		Contractcounts get(fn vpp_counts): map hasher(blake2_128_concat) T::AccountId => u64;
+		Contracts get(fn contracts): map hasher(blake2_128_concat) (T::AccountId, u64) => Option<ContractT<T>>;
+		SearchContracts get(fn searchcontracts): map hasher(blake2_128_concat) T::BlockNumber => (T::AccountId, u64);
+		Contractcounts get(fn contractcounts): map hasher(blake2_128_concat) T::AccountId => u64;
 	}
 }
 
 // The pallet's events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		SomethingStored(u32, AccountId),
+		ContractCraeted(AccountId, u64, u8),
+		ContractExecutionStatusChanged(AccountId, u64, u8),		
 	}
 );
 
@@ -58,6 +58,7 @@ decl_event!(
 decl_error! {
 	pub enum Error for Module<T: Trait> {
 		VppNotExists,
+		ContractNotExist,
 	}
 }
 
@@ -73,10 +74,9 @@ decl_module! {
 			origin, 
 			ps_addr: T::AccountId,									   //合同PS地址(通过地址和ID取得VPP所有信息)
 			vpp_number: u64,											//该地址下虚拟电厂ID
-			block_number: u64,										   //合同成交时区块号
+			block_number: u64,										   //合同创建时区块号
 			contract_price: BalanceOf<T>,		  			 //合同总价
 			energy_amount: u64,							  			 //购买电能度数
-			execution_status:u8,									   //合同执行状态
 			contract_type:bool,								 			//合同分类（购买/出售）
 			ammeter_id: Vec<u8> 									//电表编号
 		) -> dispatch::DispatchResult {
@@ -84,19 +84,23 @@ decl_module! {
 			ensure!(T::Vpp::vpp_exists(&ps_addr,vpp_number), Error::<T>::VppNotExists);
 
 			let contract_number = <Contractcounts<T>>::get(sender.clone());
+			let block_number_now = system::Module::<T>::block_number();
 			let contract_template = ContractT::<T> {
 				ps_addr: ps_addr,
 				vpp_number: vpp_number,
-				block_number: system::Module::<T>::block_number(),
+				block_number: block_number_now,
 				contract_price: contract_price,
 				energy_amount: energy_amount,
-				execution_status: execution_status,
+				execution_status: 1,
 				contract_type: contract_type,
 				ammeter_id: ammeter_id
 			};
 
 			Contracts::<T>::insert((sender.clone(), contract_number), contract_template);
-			Contractcounts::<T>::insert(sender.clone(), contract_number+1);
+			SearchContracts::<T>::insert(block_number_now, (sender.clone(), contract_number));
+			Contractcounts::<T>::insert(sender, contract_number+1);
+
+			Self::deposit_event(RawEvent::ContractCraeted(sender, contract_number, 1));
 
 			Ok(())
 		}
@@ -108,7 +112,13 @@ decl_module! {
 		) -> dispatch::DispatchResult {
 			let sender = ensure_signed(origin)?;
 
-			let contract = Contracts::<T>::get((sender.clone(), contract_number));
+			let mut contract = <Contracts<T>>::get((sender.clone(), contract_number)).ok_or(Error::<T>::ContractNotExist)?;
+
+			if(contract.execution_status != 3) {
+				contract.execution_status = 3;
+				Contracts::<T>::insert((sender.clone(), contract_number), contract);
+				Self::deposit_event(RawEvent::ContractExecutionStatusChanged(sender, contract_number, 3));
+			}
 
 			Ok(())
 		}
@@ -117,6 +127,11 @@ decl_module! {
 		pub fn completecontract(
 			origin, 
 		) -> dispatch::DispatchResult {
+			//为简化，默认同一个电表，同一时间只有一个可执行合同；
+			//合同开始执行，读取电表一个度数；
+			//OCW按周期查询该电表度数；
+			//当电表度数>=电表度数（合同开始时刻）+购买电能度数，自动将合同标记为已完成
+
 			Ok(())
 		}
 
